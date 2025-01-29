753ae08b5db3cb35849b8df95fb39864d19ec916
sabarish-vm <sabarish.science@gmail.com>
2025-01-30 00:58:27 +0100

    include python_lib path as opt

--- setup.py
+++ setup.py
@@ -3,19 +3,15 @@ from Cython.Build import cythonize
 import numpy as np
 
 
-BOOST_INCLUDE = "/opt/homebrew/include/"
+BOOST_INCLUDE = ""
+PYTHON_INCLUDE = ""
 ext_modules = [
     Extension(
         "hypPfQ",
         sources=["hyper.pyx", "wrap.cpp"],
         language="c++",
-        extra_compile_args=[
-            "-std=c++17",
-            # "-L/opt/homebrew/lib",
-            # "-lboost_system",
-            # "-I/opt/homebrew/include",
-        ],
-        include_dirs=[np.get_include(), BOOST_INCLUDE],
+        extra_compile_args=["-std=c++17"],
+        include_dirs=[np.get_include(), BOOST_INCLUDE, PYTHON_INCLUDE],
     )
 ]
 
